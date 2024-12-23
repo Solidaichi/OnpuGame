89a40e69b53b6d488b18ff59ab0fd92299d18751
Solidaichi <solideye.2014@gmail.com>
2024-12-23 10:49:25 +0900

    Add TitleController Script
    タイトル画面の他シーン遷移用スクリプトのアップロード

---
+++ TitleSceneChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneChange : MonoBehaviour
+{
+    // 遷移先シーン名を記入
+    [Header("遷移先シーン名を記入")]
+    public string sceneName;
+
+    /// <summary>
+    /// 各ボタンに合わせてシーン先を変化
+    /// 今後のボタン増加に対応
+    /// </summary>
+    public void SceneChange()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
